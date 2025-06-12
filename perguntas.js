ea19ebe2581bc2aac6cd4152c8d312312d921d14
mmariarosz <rosa.lima.maria@escola.pr.gov.br>
2025-06-12 16:39:57 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
     'Água-viva',
     'É verdade que algumas água-vivas podem brilhar no escuro?',
-    'Sim, é verdade. Não acontece com todas, mas muitas águas-vivas possuem órgãos bioluminescentes, o que as faz brilhar, sendo uma arma para distrair seus predadores'
+    'Sim, é verdade. Não acontece com todas, mas muitas águas-vivas possuem órgãos bioluminescentes, o que as faz brilhar, sendo uma arma para distrair seus predadores. '
 )
 
 criaCartao(
     'Tubarão-baleia',
-    'O tubarão-baleia é um personagem do filme Procurando Dory?
-    'Sim, é a personagem Destiny'
+    'O tubarão-baleia é um personagem do filme " Procurando Dory?",
+    'Sim a personagem Destiny'
 )
 
 criaCartao(
-    'Cavalos-marinhos',
-    'Os cavalos-marinhos vivem cerca de 25 anos?',
-    'Não, eles vivem cerca 5 anos'
+    'Cavalo-marinho',
+    'Os Cavalos-marinhos vivem cerca de 25 anos?',
+    'Não, eles vivem cerca de 5anos'
 )
 
 criaCartao(
     'Vida marinha',
-    'Qual é o maior organismo vivo da terra?',
-    'A grande Barreira de Corais'
+    'Qual o mair organismo viva da Terra?',
+    'A Grande barreira de Corais'
 )
