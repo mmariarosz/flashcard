22b0589205f2ed491fa69a29dac7257784b3569a
mmariarosz <rosa.lima.maria@escola.pr.gov.br>
2025-06-12 16:31:02 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Rosas',
-    'Qual o maior planeta do sistema solar?',
-    'Júpiter'
+    'Água-viva',
+    'É verdade que algumas água-vivas podem brilhar no escuro?',
+    'Sim, é verdade. Não acontece com todas, mas muitas águas-vivas possuem órgãos bioluminescentes, o que as faz brilhar, sendo uma arma para distrair seus predadores'
 )
 
 criaCartao(
-    'Biologia',
-    'O que é fotossíntese?',
-    'É o processo pelo qual as plantas convertem luz solar em energia'
+    'Tubarão-baleia',
+    'O tubarão-baleia é um personagem do filme Procurando Dory?
+    'Sim, é a personagem Destiny'
 )
 
 criaCartao(
-    'Química',
-    'Qual é o elemento químico mais leve?',
-    'Hidrogênio'
+    'Cavalos-marinhos',
+    'Os cavalos-marinhos vivem cerca de 25 anos?',
+    'Não, eles vivem cerca 5 anos'
 )
 
 criaCartao(
-    'Anatomia',
-    'Qual é o nome do maior osso do corpo humano?',
-    'Fêmur'
+    'Vida marinha',
+    'Qual é o maior organismo vivo da terra?',
+    'A grande Barreira de Corais'
 )
