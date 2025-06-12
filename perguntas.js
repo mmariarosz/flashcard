684a54aacc538ac960fce668416f4354799cd4fb
mmariarosz <rosa.lima.maria@escola.pr.gov.br>
2025-06-12 14:55:48 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Astronomia',
+    'Qual o maior planeta do sistema solar?',
+    'Júpiter'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é fotossíntese?',
+    'É o processo pelo qual as plantas convertem luz solar em energia'
+)
+
+criaCartao(
+    'Química',
+    'Qual é o elemento químico mais leve?',
+    'Hidrogênio'
+)
+
+criaCartao(
+    'Anatomia',
+    'Qual é o nome do maior osso do corpo humano?',
+    'Fêmur'
+)
